e59d69511a26695101fd3f63d3ba57bc0d9cee4f
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-05 01:19:23 -0800

    commit tree b2559b2e0b722489c8f998b0bd46a9153dbf63ef

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get '/name' do 
+        "My name is Loren"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Las Vegas, NV"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Reload"
+    end 
+
 end
+ 
+
+# Basic Routes Lab
+#   GET '/name'
+#     returns a 200 status code
+#     displays your name
+#   GET '/hometown'
+#     returns a 200 status code
+#     displays your hometown
+#   GET '/favorite-song'
+#     returns a 200 status code
+#     displays your favorite song
